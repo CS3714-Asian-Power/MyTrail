4a9280111a441fb21748f2b6991c25f77c6aa1d1
guhao6869 <guhao6869@gmail.com>
2017-12-12 22:48:18 -0500

    send intent service in detail activity

--- app/src/main/java/com/example/guhao/mytrail/adapter/MyAdapter.java
+++ app/src/main/java/com/example/guhao/mytrail/adapter/MyAdapter.java
@@ -52,6 +52,10 @@ public class  MyAdapter extends RecyclerView.Adapter<MyAdapter.PlaceViewHolder>{
         }
     }
 
+    public Place getPlace(int i){
+        return places.get(i);
+    }
+
     @Override
     public PlaceViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(context).inflate(R.layout.layout_card_view, parent, false);
