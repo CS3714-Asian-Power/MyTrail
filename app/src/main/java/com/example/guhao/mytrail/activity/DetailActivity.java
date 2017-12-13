d7cbd418b2eac315264953ba144373ab777789b5
guhao6869 <guhao6869@gmail.com>
2017-12-13 12:12:01 -0500

    set detail activity background image

--- app/src/main/java/com/example/guhao/mytrail/activity/DetailActivity.java
+++ app/src/main/java/com/example/guhao/mytrail/activity/DetailActivity.java
@@ -4,13 +4,18 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.guhao.mytrail.R;
@@ -18,12 +23,17 @@ import com.example.guhao.mytrail.api.DownloadHelper;
 import com.example.guhao.mytrail.api.GoogleAPIService;
 import com.example.guhao.mytrail.data.DetailPlace;
 import com.google.gson.Gson;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
 
 public class DetailActivity extends AppCompatActivity {
     private TextView test_tv;
     private String place_id;
     private ResponseReceiver receiver;
+    private AppBarLayout appbar;
+    private DownloadHelper downloadHelper;
+    private ImageView test_iv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,7 +70,7 @@ public class DetailActivity extends AppCompatActivity {
         receiver = new ResponseReceiver();
         registerReceiver(receiver,filter);
 
-        DownloadHelper downloadHelper = new DownloadHelper();
+        downloadHelper = new DownloadHelper();
         String the_url = downloadHelper.getUrlPlaceDetail(place_id);
         Intent msgIntent = new Intent(this, GoogleAPIService.class);
         msgIntent.setAction(GoogleAPIService.GET_DETAIL);
@@ -71,8 +81,9 @@ public class DetailActivity extends AppCompatActivity {
     public void initView(){
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        appbar = findViewById(R.id.app_bar);
         test_tv = findViewById(R.id.test_tv);
+//        test_iv = findViewById(R.id.test_iv);
 
     }
 
@@ -87,8 +98,31 @@ public class DetailActivity extends AppCompatActivity {
             Gson gson = new Gson();
             DetailPlace detailPlace = gson.fromJson(json, DetailPlace.class);
             Log.d("detail_activity", "onReceive: " + detailPlace.getStatus());
-
+            changeBackgroundImage(detailPlace);
         }
+    }
+
+    public void changeBackgroundImage(DetailPlace place){
+        String thumbnail_URL = downloadHelper.getPhotoURL(400, place.getResult().getPhotos().get(0).getPhoto_reference());
+        Log.d("photo_url", "changeBackgroundImage: " + thumbnail_URL);
+        Picasso.with(this).load(thumbnail_URL).into(new Target() {
+            @Override
+            public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                appbar.setBackground(new BitmapDrawable(getResources(),bitmap));
+            }
+
+            @Override
+            public void onBitmapFailed(Drawable errorDrawable) {
+
+            }
+
+            @Override
+            public void onPrepareLoad(Drawable placeHolderDrawable) {
+
+            }
+        });
+
+//        Picasso.with(this).load(thumbnail_URL).into(test_iv);
     }
 
 }
